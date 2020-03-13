71430fa51e9e55eea5d945aa46127f0a0252e301
rian santos <riansm100@gmail.com>
2020-03-13 12:54:21 -0300

    criado os metodos de circulo

--- src/main/java/exercicios/Circulo.java
+++ src/main/java/exercicios/Circulo.java
@@ -1,5 +1,27 @@
 package exercicios;
 
 public class Circulo {
+    private Double raio;
 
+    public Circulo(Double raio){
+        setRaio(raio);
+    }
+
+    public Double getRaio(){
+        return raio;
+    }
+
+    public void setRaio(Double raio){
+        if(raio<=0)
+            throw new IllegalArgumentException("valor invalido para raio");
+        this.raio = raio;
+    }
+
+    public Double calcularArea(){
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    public Double calcularPerimetro(){
+        return Math.PI * raio * 2;
+    }
 }
